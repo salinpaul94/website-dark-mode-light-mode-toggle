75fef49048e9f86cd75b110dc45b87eaa905affe
salinpaul valooran <salinpv@hotmail.com>
2023-11-02 02:07:35 +0530

    if to change moon icon to sun icon

--- version_01/index.js
+++ version_01/index.js
@@ -2,4 +2,7 @@ let icon = document.getElementById("icon");
 
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "dark theme icon/sun.png"
+  }
 }
